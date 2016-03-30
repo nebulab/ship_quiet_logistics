ea112d9ea3a9bf4ba87cd1bf70ab6bd9d25dfe04
Hugo Bastien <hugobast@gmail.com>
2016-03-30 09:56:26 -0400

    Makes prior code behave with the full url qs

--- lib/ship_quiet_logistics/sender.rb
+++ lib/ship_quiet_logistics/sender.rb
@@ -4,11 +4,11 @@ module ShipQuietLogistics
 
     def initialize(bucket)
       @sqs = AWS::SQS.new
-      @queue_name = bucket
+      @queue = bucket
     end
 
     def send_message(event_message)
-      queue = sqs.queues.named(@queue_name)
+      queue = sqs.queues[@queue]
       sqs_message = queue.send_message(event_message.to_xml)
       sqs_message.id
     end
