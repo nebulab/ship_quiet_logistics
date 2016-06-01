f49c2e7dde33374f830de5e30a4390f263aeb6ed
poulping <arnaud@godynamo.com>
2016-06-01 11:52:17 -0400

    adds accessors for the tracking attribute

--- app/models/ship_quiet_logistics/return_authorization_decorator.rb
+++ app/models/ship_quiet_logistics/return_authorization_decorator.rb
@@ -1,6 +1,7 @@
 module ShipQuietLogistics
   module ReturnAuthorizationDecorator
     def self.prepended(base)
+      attr_accessor :tracking
       class << base
         def not_pushed
           where(pushed: false)
