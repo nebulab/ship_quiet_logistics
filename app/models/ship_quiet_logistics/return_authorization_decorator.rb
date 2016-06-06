37ea9fdf058a79d4a638e96aaa7b7eb03aef0da2
poulping <arnaud@godynamo.com>
2016-06-06 16:56:34 -0400

    removes the accessor as it actually inhibits save functionality for the model

--- app/models/ship_quiet_logistics/return_authorization_decorator.rb
+++ app/models/ship_quiet_logistics/return_authorization_decorator.rb
@@ -1,7 +1,6 @@
 module ShipQuietLogistics
   module ReturnAuthorizationDecorator
     def self.prepended(base)
-      attr_accessor :tracking
       class << base
         def not_pushed
           where(pushed: false)
