155d13dc5b0ececedca61411e1f0feced2b31fae
poulping <arnaud@godynamo.com>
2016-07-18 14:44:46 -0400

    adds stub decorator for RMAs

--- spec/features/sends_rma_document_spec.rb
+++ spec/features/sends_rma_document_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe 'Sending RMA Documents', :vcr do
-  let(:rma) { create(:return_authorization)  }
+  let(:rma) { decorate(create(:return_authorization))  }
 
   it 'sends the document' do
     ShipQuietLogistics.send_rma(rma)
 
     expect(rma.reload.pushed).to be_truthy
   end
+
+  def decorate(rma)
+    RMADecoratorStub.new(rma)
+  end
+
 end
