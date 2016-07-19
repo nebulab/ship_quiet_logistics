6d018e255d1165ad993ffff41da0b3f23e94cea1
poulping <arnaud@godynamo.com>
2016-07-19 18:14:24 -0400

    adds missing require statement for the RMAResult Handler

--- lib/ship_quiet_logistics/handlers.rb
+++ lib/ship_quiet_logistics/handlers.rb
@@ -1,2 +1,3 @@
 require 'ship_quiet_logistics/handlers/error_message_handler'
+require 'ship_quiet_logistics/handlers/process_rma_handler'
 require 'ship_quiet_logistics/handlers/process_shipment_handler'
