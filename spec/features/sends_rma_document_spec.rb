a2c0454f17435214bf4dbf163741dc703abd9276
Nicolas Charlery <nicolas.charlery@gmail.com>
2017-09-28 11:30:48 -0400

    Fix RMA Document Sending Test
    The cassette had registered a specific number and date stamp. We needed then to
    reproduce the same.
    
    To debug this, I have used
    `  c.debug_logger = File.open(ARGV.first + '.log', 'w')`
    in spec/support/vcr.rb

--- spec/features/sends_rma_document_spec.rb
+++ spec/features/sends_rma_document_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 RSpec.describe 'Sending RMA Documents', :vcr do
-  let(:rma) { decorate(create(:return_authorization))  }
+  let(:rma) { decorate(create(:return_authorization, number: 'RA368041525'))  }
+
+  before do
+    allow_any_instance_of(Gentle::Documents::Request::RMADocument)
+      .to receive(:date_stamp) { '20160718_1439282' }
+  end
 
   it 'sends the document' do
     ShipQuietLogistics.send_rma(rma)
