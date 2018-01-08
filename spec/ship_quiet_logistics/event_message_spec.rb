37f6ca11ed74e0da13084454f85e2fa069c2f270
Thomas von Deyen <thomas@vondeyen.com>
2018-01-08 15:47:09 +0100

    Send given Warehouse with EventMessage
    Before the given Warehouse was set to a fixed value.

--- spec/ship_quiet_logistics/event_message_spec.rb
+++ spec/ship_quiet_logistics/event_message_spec.rb
@@ -2,10 +2,24 @@ require 'spec_helper'
 
 describe ShipQuietLogistics::EventMessage do
   subject(:xml) do
-    described_class.new('ShipmentOrder', 'test.xml', '').to_xml
+    described_class.new('ShipmentOrder', 'test.xml', config).to_xml
   end
+
+  let(:config) { Hash.new }
 
   it 'converts to xml' do
     expect(xml).to match /EventMessage/
+  end
+
+  it 'sends message for all warehouses' do
+    expect(xml).to match /Warehouse="ALL"/
+  end
+
+  context 'with specific warehouse requested' do
+    let(:config) { {'warehouse' => 'DV2'} }
+
+    it 'sends message for that warehouse' do
+      expect(xml).to match /Warehouse="DV2"/
+    end
   end
 end
